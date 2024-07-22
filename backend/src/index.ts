9fd7cda38736bbcb959dec40c390a837c6a54c28
Simon Fallon Giraldo <sfallong@eafit.edu.co>
2024-07-22 11:10:18 -0500

    Create uploads folder from index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import router from './routes';
+import path from 'path';
+import fs from 'fs'; // Add this line to import the 'fs' module
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 3000;
-
 app.use(express.json());
 app.use('/api', router);
 
+// Create the uploads directory if it doesn't exist
+const uploadPath = path.join(__dirname, '..', 'uploads');
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath);
+}
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
