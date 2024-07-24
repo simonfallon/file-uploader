372ac392f5678d2952e5c3d37d0c4131f193e9ab
Simon Fallon Giraldo <sfallong@eafit.edu.co>
2024-07-23 20:52:34 -0500

    Add heroku DB connection

--- backend/dist/db.js
+++ backend/dist/db.js
@@ -6,11 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const pool = new pg_1.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: Number(process.env.DB_PORT),
-});
+const pool = process.env.ENVIROMENT === 'local'
+    ? new pg_1.Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASS,
+        port: Number(process.env.DB_PORT),
+    })
+    : new pg_1.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
 exports.default = pool;
--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,12 +3,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: Number(process.env.DB_PORT),
-});
+const pool = process.env.ENVIROMENT === 'local'
+  ? new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: Number(process.env.DB_PORT),
+  })
+  : new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 
 export default pool;
